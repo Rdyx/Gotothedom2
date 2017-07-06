0bc49dee1f4968c6978548da0927465917e978a3
Rdyx <tiranico.pro@gmail.com>
2017-07-06 13:40:13 +0200

    finito

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
+$("#addText").text("Hello world");
 
-console.log('hello world');  
+$(".remove").remove();
+
+//$(".changeColor").css("background-color","grey");
+$(".changeColor").addClass("change");
+
+for(var boucle = 0; boucle < 4; boucle++) {
+	$(".addChildren").append("<p></p>");
+	}
+//console.log('hello world');  
